a9ad487d2609031b1be94d422f0153e421ea892a
lestafo <santiagolestaf@gmail.com>
2020-06-05 23:52:37 -0300

    todo

--- lib/main.dart
+++ lib/main.dart
@@ -1,38 +1,57 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:english_words/english_words.dart';
 
-void main() => runApp(MyApp());
+    void main(){
+      runApp(new Apee());
+    }
 
-class MyApp extends StatelessWidget {
+    class Apee extends StatelessWidget{
+      @override
+      Widget build(BuildContext context) {
+        return new MaterialApp(
+          title: 'Todo List',
+          home: new TodoList()
+              );
+      }
+      
+    }
+    class TodoList extends StatefulWidget{
+
+  @override
+  TodoListState createState() {
+    return new TodoListState();
+  }}
+
+  class TodoListState extends State<TodoList> {
+    List<String> _todoItems = [];
+
+    void _addTodoItem() {
+      setState(() {
+        int index = _todoItems.length;
+        _todoItems.add('Item' + index.toString());
+      });
+    }
+
+    Widget _buildTodoList(){
+      return new ListView.builder(itemBuilder:(context,index){
+        if(index < _todoItems.length){
+          return _buildTodoItem(_todoItems[index]);
+      }
+
+      });
+    }
+
+    Widget _buildTodoItem(String todoText){
+      return new ListTile(title: new Text(todoText));
+    }
+
+
   @override
   Widget build(BuildContext context) {
-    final wordPair = WordPair.random();
-    return MaterialApp(
-      title: 'Welcome to Flutter',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Welcome to Flutter'),
-        ),
-        body: Center(
-          child: RandomWords(),
-        ),
-      ),
+    return new Scaffold(
+      appBar: new AppBar(title: new Text('assim')),
+      body: _buildTodoList(),
+      floatingActionButton: new FloatingActionButton(onPressed: _addTodoItem, tooltip: 'Add Task',child: new Icon(Icons.add),)
     );
   }
-}
-class RandomWords extends StatefulWidget{
-  @override
-  State<StatefulWidget> createState() {
-    RandomWordsState();
-  }
 
-}
-class RandomWordsState extends State<RandomWords> {
-  @override
-  Widget build(BuildContext context) { //AH ENTENDI ISSO É UMA FUNÇÃO QUE RETORNA UM TIPO WIDGET GENIAL D++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++O>
-    final wordPair = WordPair.random();
-    return Text(wordPair.asPascalCase);
-  }
-  // TODO Add build() method
 }
